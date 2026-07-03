e1b897b7314656ee71860301fe0adca50f758a92
Lugune-dev <luguneshadrack@gmail.com>
2026-07-03 13:00:22 +0300

    some changes is done

--- servers/server.ts
+++ servers/server.ts
@@ -9,7 +9,7 @@ import fs from "fs";
 dotenv.config();
 
 const app = express();
-const PORT = 3000;
+const PORT = Number (process.env.PORT) || 3000;
 
 // Set up JSON body parser with increased limit for base64 images
 app.use(express.json({ limit: "50mb" }));
